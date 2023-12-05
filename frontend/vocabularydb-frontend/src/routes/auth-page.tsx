28bda4806b41edb10573da4384b47a468c16c510
Bill Nguyen Ton Hoang <bill.nguyentonhoang@live.com>
2023-12-05 14:12:41 +0000

    ToggleAuthenticationForm completed + Adding Mantine CSS

--- frontend/vocabularydb-frontend/src/routes/auth-page.tsx
+++ frontend/vocabularydb-frontend/src/routes/auth-page.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 
+
 function AuthPage() {
-    const [toggleAuthentication, setToggleAuthentication] = useState(false)
+
     return (
         <div>
-            
+
         </div>
     )
 }
