73e97d259215111be0fa3e15e71af9cea2d3c585
Bill Nguyen Ton Hoang <bill.nguyentonhoang@live.com>
2023-12-07 21:33:00 +0000

    Added boto3 and creating an item in DynamoDB works!

--- backend/aws-lambda/todo.py
+++ backend/aws-lambda/todo.py
@@ -1,8 +1,21 @@
+import os
+import time
+from uuid import uuid4
+import boto3
 from fastapi import FastAPI
 from mangum import Mangum
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 handler = Mangum(app)
+
+
+class PutTaskRequest(BaseModel):
+    content: str
+    user_id: Optional[str] = None
+    task_id: Optional[str] = None
+    is_done: bool = False
 
 
 @app.get("/")
@@ -10,5 +23,23 @@ def root():
     return {"message": "Hello World from Todo API"}
 
 
-# def handler():
-#     return {"message": "Hello World from Todo API"}
+@app.put("/create-task")
+async def create_task(put_task_request: PutTaskRequest):
+    created_time = int(time.time())
+    item = {
+        "user_id": put_task_request.user_id,
+        "content": put_task_request.content,
+        "is_done": False,
+        "created_time": created_time,
+        "task_id": f"task_{uuid4().hex}",
+        "ttl": int(created_time + 86400),  # Expire after 24 hours
+    }
+
+    table = _get_table()
+    table.put_item(Item=item)
+    return {"task": item}
+
+
+def _get_table():
+    table_name = os.environ.get("TABLE_NAME")
+    return boto3.resource("dynamodb").Table(table_name)
