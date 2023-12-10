34b7038dc9d4edd5a9405e9e8cab43ee5922492a
Bill Nguyen Ton Hoang <bill.nguyentonhoang@live.com>
2023-12-10 20:07:37 +0000

    Displaying tasks from AWS on frontend works!

--- frontend/vocabularydb-frontend/src/routes/home.tsx
+++ frontend/vocabularydb-frontend/src/routes/home.tsx
@@ -3,8 +3,28 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../redux-state/store';
 
 function Home() {
+    const lambdaAPI = "https://4k6jq6ypdpxyzmdnxul6i6y4ke0krgjw.lambda-url.us-east-1.on.aws/";
+
     const userId = useSelector((state: RootState) => state.user.userId);
     const [listTasks, setListTasks] = React.useState([]);
+
+    const getListTasks = async() => {
+        const response = await (await fetch(`${lambdaAPI}/list-tasks/${userId}`)).json();
+        setListTasks(response.tasks);
+    }
+
+    React.useEffect(() => {
+        getListTasks();
+    }, []);
+
+    const displayTasks = (
+        <div>
+            {listTasks.map((task) => (
+                task.content
+            ))}
+        </div>
+    );
+
 
     return (
         <div>
@@ -12,7 +32,7 @@ function Home() {
                 USER ID: {userId}
             </h1>
             <p>
-                lololololol
+                {displayTasks}
             </p>
         </div>
     )
