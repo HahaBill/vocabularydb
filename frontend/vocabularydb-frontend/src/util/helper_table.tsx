e935a4704f51ffddd8f475d041e933044c36dc78
Bill Nguyen Ton Hoang <bill.nguyentonhoang@live.com>
2023-12-30 17:33:29 +0000

    Implemented filterData and sortData functions

---
+++ frontend/vocabularydb-frontend/src/util/helper_table.tsx
@@ -0,0 +1,31 @@
+import { keys } from "@mantine/core";
+import TableRowData from "../model/TableRowData";
+
+function filterData(data: TableRowData[], search: string) {
+    const query = search.toLowerCase().trim();
+    return data.filter((item) =>
+      keys(data[0]).some((key) => item[key].toLowerCase().includes(query))
+    );
+}
+
+function sortData(data: TableRowData[], 
+    payload: { sortBy: keyof TableRowData | null; reversed: boolean; search: string }) {
+    const { sortBy } = payload;
+  
+    if (!sortBy) {
+      return filterData(data, payload.search);
+    }
+  
+    return filterData(
+      [...data].sort((a, b) => {
+        if (payload.reversed) {
+          return b[sortBy].localeCompare(a[sortBy]);
+        }
+  
+        return a[sortBy].localeCompare(b[sortBy]);
+      }),
+      payload.search
+    );
+}
+
+export default { filterData, sortData };
